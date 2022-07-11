26d3cd7c71f9589732af43d227d572ba04f0e9e7
SuperRazorDev <micahwright560@gmail.com>
2022-07-11 20:22:28 +0800

    feat/list: List component with styled table and sort by name feature

--- src/views/Brewery/List.jsx
+++ src/views/Brewery/List.jsx
@@ -1,19 +1,107 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./List.css";
 
 export default function BreweryList() {
+  const [breweries, setBreweries] = useState([]);
+  const [query, setQuery] = useState("");
+  const [sorting, setSorting] = useState("ascending");
+
+  const fetchBreweries = (query) => {
+    fetch(
+      query
+        ? ` https://api.openbrewerydb.org/breweries/search?query=${query}&per_page=10`
+        : `https://api.openbrewerydb.org/breweries?per_page=10`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort(
+          (a, b) =>
+            (sorting === "ascending" ? 1 : -1) * a.name.localeCompare(b.name)
+        );
+        setBreweries(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchBreweries("");
+  }, []);
+
+  useEffect(() => {
+    sortBreweries();
+  }, [sorting]);
+
+  const sortBreweries = () => {
+    const sortedbreweries = [...breweries].sort(
+      (a, b) =>
+        (sorting === "ascending" ? 1 : -1) * a.name.localeCompare(b.name)
+    );
+    setBreweries(sortedbreweries);
+  };
+
+  const onReset = () => {
+    setQuery("");
+    fetchBreweries("");
+  };
+
+  const onSearch = () => {
+    fetchBreweries(query);
+  };
+
+  const toggleSorting = () => {
+    setSorting(sorting === "ascending" ? "descending" : "ascending");
+  };
+
   return (
     <main>
       <h1>Brewery Catalog</h1>
-      <form>
-        <input type='text' name='search' placeholder='Find a brewery' />
-        <button type='submit'>Search</button>
-        <button type='reset'>Reset</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          name="search"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          placeholder="Find a brewery"
+        />
+        <button onClick={onSearch}>Search</button>
+        <button onClick={onReset}>Reset</button>
       </form>
-      <ul>
-        <li>
-          <Link to='/breweries/1'>Brewery 1</Link> - Brewtown, OR
-        </li>
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <button type="button" className={sorting} onClick={toggleSorting}>
+                Name
+              </button>
+            </th>
+            <th>
+              <button type="button">City</button>
+            </th>
+            <th>
+              <button type="button">State</button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {breweries.map((brewery, index) => (
+            <tr key={brewery.id}>
+              <td>
+                <Link to={`/breweries/${brewery.id}`}>{brewery.name}</Link>
+              </td>
+              <td>{brewery.city}</td>
+              <td>{brewery.state}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <ul></ul>
     </main>
   );
 }
